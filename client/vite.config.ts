492493b60bb5351be436cd58de1812898704455f
lienard maxence <lienardmaxence03@gmail.com>
2025-11-11 15:59:23 +0100

    correction pour deploiement

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    base: "/DeployWeb2025_2026/", // à modifier selon le nom du dépôt GitHub
     root: ".", // facultatif, "." = le dossier client
     server: {
         port: 5173,
